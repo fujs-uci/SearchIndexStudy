1bce2e32426bf411f164be99e0eb63a0b0d2da78
Home-Fu <fu.justin.s@gmail.com>
2020-10-02 14:29:41 -0700

    Finished Template

--- results/views.py
+++ results/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import SearchQuery
 from results.search_index import SearchIndexWrapper
+from .models import Movies
 
 
 def index(request):
@@ -18,6 +19,7 @@ def index(request):
     if request.method == "POST":
         search_query = SearchQuery(request.POST)
         search_results = search_index.lookup(search_query['query'].value(), limit=10)
+        search_results = Movies.objects.filter(id__in=search_results)
     else:
         search_query = SearchQuery()
     template = "results/index.html"
